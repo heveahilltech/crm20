9622bd07cb1b3378f798a4776a94548253515941
akhilprasanthh <akhilp@heveahill.com>
2026-02-24 05:46:55 -0600

    Icon drop down fix

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/MultiWorkspaceDropdown/internal/MultiWorkspaceDropdownClickableComponent.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/MultiWorkspaceDropdown/internal/MultiWorkspaceDropdownClickableComponent.tsx
@@ -1,15 +1,12 @@
-import { DEFAULT_WORKSPACE_LOGO } from '@/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo';
 import { NavigationDrawerAnimatedCollapseWrapper } from '@/ui/navigation/navigation-drawer/components/NavigationDrawerAnimatedCollapseWrapper';
 import {
   StyledContainer,
   StyledIconChevronDown,
-  StyledLabel,
 } from '@/ui/navigation/navigation-drawer/components/MultiWorkspaceDropdown/internal/MultiWorkspacesDropdownStyles';
 import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
 import { useRecoilValue } from 'recoil';
 import { useTheme } from '@emotion/react';
 import { isNavigationDrawerExpandedState } from '@/ui/navigation/states/isNavigationDrawerExpanded';
-import { Avatar } from 'twenty-ui/display';
 import styled from '@emotion/styled';
 
 const StyledLogo = styled.img`
@@ -33,9 +30,6 @@ export const MultiWorkspaceDropdownClickableComponent = () => {
     >
       <StyledLogo src="/images/integrations/VoxRing.png" alt="VoxRing Logo" />
 
-      <NavigationDrawerAnimatedCollapseWrapper>
-        <StyledLabel>{currentWorkspace?.displayName ?? ''}</StyledLabel>
-      </NavigationDrawerAnimatedCollapseWrapper>
       <NavigationDrawerAnimatedCollapseWrapper>
         <StyledIconChevronDown
           size={theme.icon.size.md}
