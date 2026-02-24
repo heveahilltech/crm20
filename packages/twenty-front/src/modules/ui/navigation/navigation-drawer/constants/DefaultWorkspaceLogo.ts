cd30a9e1e9f11bc4f487f3c0b0adb3448768bd1e
akhilprasanthh <akhilp@heveahill.com>
2026-02-24 07:10:35 -0600

    logo name change

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
@@ -2,4 +2,4 @@ export const DEFAULT_WORKSPACE_LOGO =
   '/images/integrations/VoxRing.png';
 
 export const VoxRing_Icon = '/images/integrations/VoxringIcon.png';
-export const VoxRing_FavIcon = '/images/integrations/VoxRingFavIcon.png';
+export const VoxRing_FavIcon = '/images/integrations/VoxRingFavicon.png';
