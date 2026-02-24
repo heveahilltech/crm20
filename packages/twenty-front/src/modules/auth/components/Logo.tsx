2941ec6db16fc225288c54a799a972c18c3bb6e2
akhilprasanthh <akhilp@heveahill.com>
2026-02-24 06:45:30 -0600

    logo icon fix

--- packages/twenty-front/src/modules/auth/components/Logo.tsx
+++ packages/twenty-front/src/modules/auth/components/Logo.tsx
@@ -6,7 +6,7 @@ import { Avatar } from 'twenty-ui/display';
 import { UndecoratedLink } from 'twenty-ui/navigation';
 import { REACT_APP_SERVER_BASE_URL } from '~/config';
 import { useRedirectToDefaultDomain } from '~/modules/domain-manager/hooks/useRedirectToDefaultDomain';
-
+import { VoxRing_Icon } from '@/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo';
 
 type LogoProps = {
   primaryLogo?: string | null;
@@ -58,7 +58,7 @@ export const Logo = ({
   onClick,
 }: LogoProps) => {
   const { redirectToDefaultDomain } = useRedirectToDefaultDomain();
-  const defaultPrimaryLogoUrl = `${window.location.origin}/images/integrations/VoxRingIcon.png`;
+  const defaultPrimaryLogoUrl = `${window.location.origin}${VoxRing_Icon}`;
 
   const primaryLogoUrl = getImageAbsoluteURI({
     imageUrl: primaryLogo ?? defaultPrimaryLogoUrl,
