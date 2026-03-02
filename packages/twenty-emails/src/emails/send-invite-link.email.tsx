5f84331b932e64d06e23c9635fe6535acfc7d20c
akhilprasanthh <akhilp@heveahill.com>
2026-03-02 05:16:27 -0600

    email content changes to voxring

--- packages/twenty-emails/src/emails/send-invite-link.email.tsx
+++ packages/twenty-emails/src/emails/send-invite-link.email.tsx
@@ -9,7 +9,6 @@ import { HighlightedText } from 'src/components/HighlightedText';
 import { Link } from 'src/components/Link';
 import { MainText } from 'src/components/MainText';
 import { Title } from 'src/components/Title';
-import { WhatIsTwenty } from 'src/components/WhatIsTwenty';
 import { capitalize } from 'src/utils/capitalize';
 import { createI18nInstance } from 'src/utils/i18n.utils';
 import { type APP_LOCALES } from 'twenty-shared/translations';
@@ -45,7 +44,7 @@ export const SendInviteLinkEmail = ({
 
   return (
     <BaseEmail width={333} locale={locale}>
-      <Title value={i18n._('Join your team on Twenty')} />
+      <Title value={i18n._('Join your team on VoxRing')} />
       <MainText>
         <Trans
           id="{senderName} (<0>{senderEmail}</0>) has invited you to join a workspace called <1>{workspaceName}</1>."
@@ -77,7 +76,6 @@ export const SendInviteLinkEmail = ({
         {workspace.name ? <HighlightedText value={workspace.name} /> : <></>}
         <CallToAction href={link} value={i18n._('Accept invite')} />
       </HighlightedContainer>
-      <WhatIsTwenty i18n={i18n} />
     </BaseEmail>
   );
 };
