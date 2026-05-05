8b17116d0e045e79d2db61602ff50bc66a925faf
akhilprasanthh <akhilp@heveahill.com>
2026-05-05 08:53:07 -0600

    alingment correction

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawer.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawer.tsx
@@ -60,7 +60,7 @@ const StyledContainer = styled.div<{
   isMobile?: boolean;
   isExpanded?: boolean;
 }>`
-  background: #ffffff;  
+  background: ${themeCssVariables.background.primary}; 
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -81,11 +81,11 @@ const StyledContainer = styled.div<{
   }
 
   &[data-color-scheme='light'] {
-    background: #ffffff;
+    background: ${themeCssVariables.background.primary};
   }
 
   &[data-color-scheme='dark'] {
-    background: #000000;
+    background: ${themeCssVariables.background.primary};
   }
 `;
 
